69455a5bf079617d9a6945c7cbfedb9e8b45d2b6
Corey <flores.bmichelle@ufl.eduCoreygit config --global user.name Coreygit pushlsgit config --global user.email flores.bmichelle@ufl.eduCorey>
2024-03-27 14:54:02 -0400

    rendered calendar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,21 @@ import Modal from 'react-modal'
 import Home from "./Components/Home"
 import Login from "./Components/Login"
 import Signup from "./Components/Signup"
-import calendar from "./Components/calendar"
+import Calendar from "./Components/Cal"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login/>}/>
-          <Route path="/signup" element={<Signup/>}/>
-          <Route path="/home" element={<Home/>}/>
-        </Routes>
-      </Router>
+   <Router>
+  <Routes>
+    <Route path="/" element={<Login/>}/>
+    <Route path="/signup" element={<Signup/>}/>
+    <Route path="/home" element={<Home/>}/>
+    <Route path="/calendar" element={<Calendar/>}/>
+  </Routes>
+</Router>
     </div>
   );
 }
---
+++ frontend/src/Components/Cal.js
@@ -0,0 +1,11 @@
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid' 
+
+export default function Calendar() {
+  return (
+    <FullCalendar
+      plugins={[ dayGridPlugin ]}
+      initialView="dayGridMonth"
+    />
+  )
+}
