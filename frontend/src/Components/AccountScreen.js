9d2a2cb90e001e2b3b107e326d0fc1f32097b08c
Corey <flores.bmichelle@ufl.eduCoreygit config --global user.name Coreygit pushlsgit config --global user.email flores.bmichelle@ufl.eduCorey>
2024-04-14 13:17:46 -0400

    added name box and email box

--- frontend/src/Components/AccountScreen.js
+++ frontend/src/Components/AccountScreen.js
@@ -3,6 +3,9 @@ import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Style/AccountScreen.css";
+import { useNavigate} from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios"
 
 const AccountScreen = () => {
     return (
@@ -11,7 +14,17 @@ const AccountScreen = () => {
                     <button id="returnhome-button">Home</button>
                 </Link>
             <h1>My Account</h1>
+<div className="account-container">
+      <div className="account-box">
+        <span className="placeholder-text">Username: JohnDoe</span>
+      </div>
+      <div className="account-box">
+        <span className="placeholder-text">Email: johndoe@example.com</span>
+      </div>
+      {/* Add more boxes with placeholder text as needed */}
+    </div>
         </div>
+        
     );
 };
 
