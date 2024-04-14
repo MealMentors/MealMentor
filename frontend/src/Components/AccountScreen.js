3a561eb7baaa197153aeaf87c84d59484b69c7bd
Corey <flores.bmichelle@ufl.eduCoreygit config --global user.name Coreygit pushlsgit config --global user.email flores.bmichelle@ufl.eduCorey>
2024-04-14 13:05:44 -0400

    added return home button to account page

--- frontend/src/Components/AccountScreen.js
+++ frontend/src/Components/AccountScreen.js
@@ -1,11 +1,16 @@
 // AccountScreen.js
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Style/AccountScreen.css";
 
 const AccountScreen = () => {
     return (
         <div>
-            <h1>New Page</h1>
-            <p>This is a new page content.</p>
+            <Link to="/home">
+                    <button id="returnhome-button">Home</button>
+                </Link>
+            <h1>My Account</h1>
         </div>
     );
 };
